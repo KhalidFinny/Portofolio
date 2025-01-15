e02d45fc6b9a9cedfb45f31e4ab9440db5113310
Muhammad Khalid Atthoriq <141740987+KhalidFinny@users.noreply.github.com>
2025-01-15 12:17:08 +0700

    supabase setup for programming is done

--- src/SupabaseClient.js
+++ src/SupabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lpidxbttworqrgngoppg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwaWR4YnR0d29ycXJnbmdvcHBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUxNjg2NjAsImV4cCI6MjA1MDc0NDY2MH0.wYlGGPQD6O3AgIWenJSmvB4ZQUpoSJRrzSkMNBVlzBE';
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
