d8c374c53eb1731e566a67d197c75549b8526005
finny <katthoriq@gmail.com>
2025-03-09 06:15:41 +0700

    remove unused image assets and update image paths in components

--- src/globalstate.js
+++ src/globalstate.js
@@ -3,10 +3,10 @@ document.addEventListener('alpine:init', () => {
     Alpine.store('global', {
       selectedTeam: null,
       teamColors: {
-        warriors: { DEFAULT: '#1D428A', secondary: '#FFC72C', logo: '/src/assets/Images/Currylogo.webp' },
-        'mclaren-2024': { DEFAULT: '#FF8700', secondary: '#000000', logo: '/src/assets/Images/Landologo.webp' },
-        'malboro-mclaren': { DEFAULT: '#FF0000', secondary: '#FFFFFF', logo: '/src/assets/Images/Sennalogo.webp' },
-        'hakkinen-mclaren': { DEFAULT: '#C0C0C0', secondary: '#000000', logo: '/src/assets/Images/Mikalogo.webp' }
+        warriors: { DEFAULT: '#1D428A', secondary: '#FFC72C', logo: '/Images/Currylogo.webp' },
+        'mclaren-2024': { DEFAULT: '#FF8700', secondary: '#000000', logo: '/Images/Landologo.webp' },
+        'malboro-mclaren': { DEFAULT: '#FF0000', secondary: '#FFFFFF', logo: '/Images/Sennalogo.webp' },
+        'hakkinen-mclaren': { DEFAULT: '#C0C0C0', secondary: '#000000', logo: '/Images/Mikalogo.webp' }
       }
     });
   });
