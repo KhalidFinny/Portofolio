973be14c7b78fdefd63627de992cf1a37ac6f1b7
finny <katthoriq@gmail.com>
2025-03-09 06:12:40 +0700

    refactor Navbar and about me components for improved styling; update Videography page structure and add new video data

--- src/videodata.js
+++ src/videodata.js
@@ -8,8 +8,32 @@ export const youtubeVideos = [
       title: "BrainBoost: Fully unlock your true potential",
     },
     {
-      id: "a-H6iiEsaqg?si",
+      id: "a-H6iiEsaqg",
       title: "Maba ARC part 1 | This Is The Beginning, We Aren't Done Yet. WRI, Xplore!",
+    },
+    {
+      id: "fH4F6tah8PY",
+      title: "WRI in a Nutshell, Get to Know us ",
+    },
+    {
+      id: "irF6L604Dfs",
+      title: "Maba ARC part 2 | An Early Access to Know us Better",
+    },
+    {
+      id: "BbavD9WJSHI",
+      title: "Maba ARC part 3 | Welcome New Xplorers",
+    },
+    {
+      id: "gGdV7M1CdPM",
+      title: "Dream SMP Monologue after the Manberg War",
+    },
+    {
+      id: "L8FidX4G_rc",
+      title: "Messy Desk",
+    },
+    {
+      id: "eRw_8OFwiw4",
+      title: "Pameran Seni Budaya XII Mipa 1",
     }
   ];
   
@@ -19,54 +43,65 @@ export const instagramReels = [
       title: "The Quiet Pagas",
     },
     {
-      id: "IG_REEL_2",
-      title: "",
+      id: "CdSl32UquPr",
+      title: "Pride",
     },
     {
-      id: "IG_REEL_2",
-      title: "Instagram Reel 2",
+      id: "Cht1FlSInvG",
+      title: "IBBS Time",
     },
     {
-      id: "IG_REEL_2",
-      title: "Instagram Reel 2",
+      id: "Chzo1NmKTt8",
+      title: "IBBS Part 2",
     },
     {
-      id: "IG_REEL_2",
-      title: "Instagram Reel 2",
+      id: "Ch_MUKmoH0n",
+      title: "IBBS Part 3",
     },
     {
-      id: "IG_REEL_2",
-      title: "Instagram Reel 2",
+      id: "Ciq8vFaoxjr",
+      title: "IBBS Semi Finals",
     },
     {
-      id: "IG_REEL_2",
-      title: "Instagram Reel 2",
+      id: "CisQHaDqSRM",
+      title: "IBBS Finals",
     },
     {
-      id: "IG_REEL_2",
-      title: "Instagram Reel 2",
+      id: "CiunYGuIEK_",
+      title: "IBBS The All Star Game",
     },
     {
-      id: "IG_REEL_2",
-      title: "Instagram Reel 2",
+      id: "CkVacGrK1YT",
+      title: "The 3 Musketeers",
     },
     {
-      id: "IG_REEL_2",
-      title: "Instagram Reel 2",
+      id: "CnByUgloCN3",
+      title: "Ken Block Tribute",
     },
     {
-      id: "IG_REEL_2",
-      title: "Instagram Reel 2",
+      id: "C-L9v7NSTGC",
+      title: "Timeless Beauty",
     },
     {
-      id: "IG_REEL_2",
-      title: "Instagram Reel 2",
+      id: "DAmfv2VNm3b",
+      title: "Oprec WRI",
     },
     {
-      id: "IG_REEL_2",
-      title: "Instagram Reel 2",
+      id: "DFJzMuaSTo3",
+      title: "Personal Top 5",
+    },    
+    {
+      id: "DDb7QshzSJ2",
+      title: "WRI x Kuy Studio",
     },
-    
+    {
+      id: "CpK5IKzpGZ5",
+      title: "Ruang Sinau 7 Opening",
+    },
+    {
+      id: "CrBnxXTLOOt",
+      title: "Ruang Sinau 7 Recap",
+    },   
   ];
   export default {youtubeVideos, instagramReels};
   
